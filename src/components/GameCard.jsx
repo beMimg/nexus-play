2a2ad644350457d1f7b151a7bcbe1836d8b50c62
beMimg <bernardommguerreiro@gmail.com>
2024-02-01 13:42:37 +0100

    small ui change to GameCard

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaCartArrowDown } from "react-icons/fa";
 
 export default function GameCard({ handleAddBtn, game }) {
   const [quantity, setQuantity] = useState(1);
@@ -12,24 +13,23 @@ export default function GameCard({ handleAddBtn, game }) {
   };
 
   return (
-    <div className="flex flex-col  gap-4 rounded border-4 border-black bg-slate-800 p-2 pb-4 text-white shadow-2xl">
+    <div className="flex flex-col shadow-2xl">
       <img
         src={game.background_image}
         alt={game.name + "image"}
-        className=" h-24 w-full object-cover object-center pb-2 pt-2 lg:h-48"
+        className=" h-24 w-full object-cover object-center  lg:h-48"
       />
-      <div className="flex flex-row justify-between">
-        <h1 className="font-bold">{game.name}</h1>
-        <p className="font-bold">${game.id}</p>
-      </div>
-      <div className="grid grid-cols-2 items-center">
-        <input
-          type="number"
-          value={quantity}
-          onChange={handleQuantityChange}
-          className="h-12 rounded bg-slate-200 text-black"
-          min={1}
-        />
+      <div className="flex flex-row  items-center justify-between bg-gray-950 p-4 text-white">
+        <div className="flex flex-col gap-2">
+          <h1 className="font-bold">{game.name}</h1>
+          <input
+            type="number"
+            value={quantity}
+            onChange={handleQuantityChange}
+            className="h-12 rounded bg-slate-200 text-black"
+            min={1}
+          />
+        </div>
         {quantity > 0 ? (
           <button
             onClick={() =>
@@ -40,7 +40,7 @@ export default function GameCard({ handleAddBtn, game }) {
           >
             <span className="top-0 flex h-full w-full items-center justify-center before:absolute before:bottom-0 before:left-1/4 before:z-0 before:h-0 before:w-1/4 before:bg-black before:duration-500 after:absolute after:right-1/4 after:top-0 after:z-0 after:h-0 after:w-1/4 after:bg-black after:duration-500 hover:text-white group-hover:before:h-full group-hover:after:h-full"></span>
             <span className="  absolute bottom-0 left-0 right-0 top-0 z-10 flex h-full w-full items-center justify-center group-hover:text-white">
-              Add
+              <FaCartArrowDown className="mr-2" /> {game.id}
             </span>
           </button>
         ) : (
